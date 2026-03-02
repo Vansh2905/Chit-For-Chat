9236f8363c7bbed35bce15b5cecd4664bb0b078b
Vansh2905 <vanshahluwalia29@gmail.com>
2026-03-02 21:36:39 +0530

    Google sign in

--- client/src/app/api/auth/[...nextauth]/route.js
+++ client/src/app/api/auth/[...nextauth]/route.js
@@ -9,6 +9,24 @@ const handler = NextAuth({
     }),
   ],
   secret: process.env.NEXTAUTH_SECRET,
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      return true;
+    },
+    async redirect({ url, baseUrl }) {
+      return url.startsWith(baseUrl) ? url : baseUrl;
+    },
+    async session({ session, token }) {
+      return session;
+    },
+    async jwt({ token, user }) {
+      return token;
+    },
+  },
+  pages: {
+    signIn: '/Login',
+    error: '/Login',
+  },
 });
 
 export { handler as GET, handler as POST };
